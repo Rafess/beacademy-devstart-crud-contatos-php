38e10d3a15be21e8712f9c7b05e926d0f88530dc
Rafes <rafadvcoliveira@gmail.com>
2022-06-05 22:27:11 -0300

    Create actions.php

---
+++ actions.php
@@ -0,0 +1,16 @@
+<?php 
+function home() {
+    include 'screens/home.php';
+}
+function login() {
+    include 'screens/login.php';
+}
+function listPage() {
+    include 'screens/list.php';
+}
+function register() {
+    include 'screens/register.php';
+}
+function error404()  {
+    include 'screens/404.php';
+}
