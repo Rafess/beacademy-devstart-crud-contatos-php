e96ac79df2278c546e33b681a13703f6b0c4267c
Rafes <rafadvcoliveira@gmail.com>
2022-06-05 22:30:07 -0300

    Create register

---
+++ screens/register.php
@@ -0,0 +1,24 @@
+    <div class="container rounded-3 p-3 text-light mt-3" style="background-color: #7532F9;">
+    <h1 class="h1 text-center mb-3 p-3"> Cadastro </h1>
+            <form class="row g-3">
+            <div class="col-md-6">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="col-md-6">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="col-12">
+                <label for="name" class="form-label">Nome Completo</label>
+                <input type="text" class="form-control" id="name" name="name">
+            </div>
+            <div class="col-12">
+                <label for="telephone" name="telephone"class="form-label">Telefone</label>
+                <input type="text" class="form-control" id="telephone" name="telephone" placeholder="(DDD) 9 .....">
+            </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button type="submit" class="btn btn-light me-md-2 text-light rounded-pill" style="background-color: #F844AD; border: 1px solid #F844AD">Cadastrar</button>
+            </div>
+        </form>
+    </div>
