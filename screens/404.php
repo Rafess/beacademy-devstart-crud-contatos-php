47f455e1ccea64cedddd1d9148889685a7926149
Rafes <rafadvcoliveira@gmail.com>
2022-06-05 22:29:07 -0300

    Create list.php

---
+++ screens/404.php
@@ -0,0 +1,6 @@
+<div class="container rounded-3 p-5 mt-2" style="background-color: #7532F9;">
+    <h1 class="dislplay-1 text-center text-light">
+        404 - PAGE NOT FOUND
+    </h1>
+    <img src="./assets/404.svg" alt="on the moon" width="80%"/>
+</div>
